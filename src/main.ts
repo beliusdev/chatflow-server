5a6713216a012cbef3db394868a2d1b602c960d7
beliusdev <121978627+beliusdev@users.noreply.github.com>
2024-07-11 23:30:53 +0400

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,6 @@ import { AppModule } from "./app.module";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({ origin: "*" }); // todo
-  await app.listen(5000, {host: "0.0.0.0"});
+  await app.listen(5000, "0.0.0.0");
 }
 bootstrap();
